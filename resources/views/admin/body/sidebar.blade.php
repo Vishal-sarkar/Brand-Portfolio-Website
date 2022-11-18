7f054f462a8d7e801ba220e2762c01fc59161578
Vishal sarkar <74032678+Vishal-sarkar@users.noreply.github.com>
2022-11-18 23:08:27 +0530

    change sidebar

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -2,18 +2,7 @@
 
     <div data-simplebar class="h-100">
 
-        <!-- User details -->
-        <div class="user-profile text-center mt-3">
-            <div class="">
-                <img src="{{ asset('backend/assets/images/users/avatar-1.jpg') }}" alt=""
-                    class="avatar-md rounded-circle">
-            </div>
-            <div class="mt-3">
-                <h4 class="font-size-16 mb-1">Julia Hudda</h4>
-                <span class="text-muted"><i class="ri-record-circle-line align-middle font-size-14 text-success"></i>
-                    Online</span>
-            </div>
-        </div>
+        
 
         <!--- Sidemenu -->
         <div id="sidebar-menu">
